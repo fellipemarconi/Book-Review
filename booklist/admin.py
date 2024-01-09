b78183a5eb43a9b1eee7cbe0c73f40eb32e18eda
Fellipe <anksrr@proton.me>
2024-01-09 18:56:54 -0300

    It was created configs to BookAdmin in admin.py

--- booklist/admin.py
+++ booklist/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from booklist import models
 
 # Register your models here.
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'year', 'created_at')
+    ordering = '-id',
+    list_filter = 'created_at',
+    search_fields = ('id', 'title', 'genre', 'year',)
+    list_per_page = 25
+    list_max_show_all = 100
