8799846a5b2d429b938329067138df789b70d174
Fellipe <anksrr@proton.me>
2024-01-09 12:09:41 -0300

    I created the logic and authenticate to login with user

--- booklist/views/user_forms.py
+++ booklist/views/user_forms.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth, messages
 
 # Create your views here.
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('booklist:index')
+    
+    if request.method == "POST":
+        username = request.POST.get('user')
+        password = request.POST.get('password')
+        
+        user = auth.authenticate(
+            request, 
+            username=username, 
+            password=password
+        )
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Login Successful!')
+            return redirect('booklist:index')
+        messages.error(request, "Login Invalid!")
+        
     return render(request, 'booklist/login.html')
 
