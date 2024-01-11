0447e41d448d699ed09a793375bc7b631ac00224
Fellipe <anksrr@proton.me>
2024-01-10 22:44:11 -0300

    It was create a pagination and added in template index

--- booklist/views/booklist_views.py
+++ booklist/views/booklist_views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from booklist.models import Book
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
     books = Book.objects.all().order_by('title')
     
+    paginator = Paginator(books, 15)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'books': books,
+        'page_obj': page_obj,
     }
     
     return render(request, 'booklist/index.html', context)
