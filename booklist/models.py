be56dee8c6b9388de04e1fd6c236cb2838684b48
Fellipe <anksrr@proton.me>
2024-01-12 02:44:57 -0300

    Updated the models include picture and author

--- booklist/models.py
+++ booklist/models.py
@@ -7,6 +7,8 @@ class Book(models.Model):
     description = models.CharField(max_length=255)
     year = models.IntegerField()
     genre = models.CharField(max_length=30)
+    author = models.CharField(max_length=50, null=True)
+    picture = models.ImageField(null=True, blank=True, upload_to='pictures/%Y/%m/')
     
     def __str__(self):
         return f"{self.title}"
--- project/settings.py
+++ project/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATIC_ROOT = BASE_DIR / 'static_collected'
 
 MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
